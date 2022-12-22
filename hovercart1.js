b33ab018548a9f9fe2176657960e2893cec17ee6
osama khan <osk7462@gmail.com>
2022-12-22 14:33:27 +0530

    Create hovercart1.js

---
+++ hovercart1.js
@@ -0,0 +1,14 @@
+console.log("hovercart is loaded")
+$(document).ready(function (marketplace) {
+        $.ajax({
+            url: "https://ipinfo.io/json",
+            success: function (data) {
+                    console.log(data)
+                if (data.region == "Illinois") {
+                    marketplace = "52a4d104-0875-4fb7-b02e-804c85d79430"; 
+                    console.log(marketplace)
+                    $.getScript("https://hovercart.quiverstest.com/?Marketplace=" + marketplace, function () { })
+                }
+            }
+        });
+});
