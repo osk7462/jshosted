bf5bb7b635a05308d062fcf7521671c484a2ab93
osama khan <osk7462@gmail.com>
2022-12-22 15:16:14 +0530

    Update HovercartI.js

--- HovercartI.js
+++ HovercartI.js
@@ -1,9 +1,15 @@
 console.log("hovercart is loaded")
 if ('undefined' === typeof jQuery){
+  (function() {
   const script = document.createElement("script");
   script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
   script.type = 'text/javascript';
+  script.addEventListener('load', () => {
+    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+    // use jQuery below
+  });
   document.head.appendChild(script);
+})();
 }
 
 $(document).ready(function (marketplace) {
