06da428ed6564df27e11e003ca54d27c4f191771
osama khan <osk7462@gmail.com>
2022-12-23 11:33:20 +0530

    Create HoverCart8.js

---
+++ HoverCart8.js
@@ -0,0 +1,42 @@
+
+console.log("loading HoverCart")
+if (window.jQuery && typeof window.jQuery == "function" && window.jQuery() !== null) {
+   if (!$) {
+    $ = window.jQuery
+   }
+   loadHoverCart()
+} else {
+    (function () {
+        const script = document.createElement("script");
+        script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+        script.type = 'text/javascript';
+        script.addEventListener('load', () => {
+            console.log("JQuery is loaded")
+            loadHoverCart()
+        });
+        document.head.appendChild(script);
+    })();
+}
+
+function loadHoverCart() {
+    $(document).ready(function (marketplace) {
+        $.ajax({
+            url: "https://ipinfo.io/json",
+            success: function (data) {
+                switch (data.region) {
+                    case "Illinois":
+                        marketplace = "52a4d104-0875-4fb7-b02e-804c85d79430";
+                        break;
+                    case "California":
+                        marketplace = "52a4d104-0875-4fb7-b02e-804c85d79430";
+                        break;
+                    default:
+                        marketplace = ""
+                }
+                console.log("marketplace: ", marketplace)
+                if (marketplace)
+                    $.getScript("https://hovercart.quiverstest.com/?Marketplace=" + marketplace, function () { })
+            }
+        })
+    });
+}
